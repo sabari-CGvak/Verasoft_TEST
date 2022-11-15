b4cbf8ffdc90c45b51b32f0ef5909aee31310ef7
sabariesh <sabariesh.a@cgvakindia.com>
2022-11-16 01:14:37 +0530

    latest changes for UI updates

--- src/Pages/Orders/index.tsx
+++ src/Pages/Orders/index.tsx
@@ -102,49 +102,49 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
 
       {/* =============== Grid start =============== */}
       <div className="grid_container">
-        <div className="grid_container_row">
+        <div className="grid_container_row wid_97px">
           <div className="user_profile">
             <img src={IMG_user} />
             <p>{summary?.data.gender} - {(new Date().getFullYear()) - Number(summary?.data.birth_date?.substring(0, 4))}</p>
           </div>
         </div>
-        <div className="grid_container_row">
+        <div className="grid_container_row wid_239px">
           <div className="user_contact_details">
             <ul>
-              <li><img src={IMG_SmartPhone} /><div className="content">{Number(summary?.data?.id)}</div>  </li>
-              <li><img src={IMG_SmartPhone} /><div className="content">{summary?.data?.home_phone}</div>  </li>
-              <li><img src={IMG_Appartments} /><div className="content">{summary?.data?.mobile_phone}</div>  </li>
-              <li><img src={IMG_Home} /><div className="content">{summary?.data?.work_phone}</div>  </li>
-              <li><img src={IMG_Arroba} /><div className="content">{summary?.data?.email}</div>  </li>
+              <li><img src={IMG_Prouser} /><div className="content">{Number(summary?.data?.id)}</div>  </li>
+              <li><img src={IMG_SmartPhone} className="smart_ph_icn"/><div className="content">{summary?.data?.home_phone}</div>  </li>
+              <li><img src={IMG_Appartments} className="wid_12px"/><div className="content">{summary?.data?.mobile_phone}</div>  </li>
+              <li><img src={IMG_Home} className="img_home_icn" /><div className="content">{summary?.data?.work_phone}</div>  </li>
+              <li><img src={IMG_Arroba} className="image_arroba_icn"/><div className="content">{summary?.data?.email}</div>  </li>
             </ul>
           </div>
         </div>
-        <div className="grid_container_row" style={{ padding: '0px' }}>
+        <div className="grid_container_row pad_0px">
           <div className="communication_activity_body">
             <div className="title">90-DAY COMMUNICATION ACTIVITY</div>
             <div className="content">
-              <div className="card border_right_1_white">
+              <div className="card border_right_1_white wid_185px">
                 <div className="number">{Number(summary?.data.activity?.sms)}</div>
                 <div className="data">sms</div>
               </div>
-              <div className="card border_right_1_white">
+              <div className="card border_right_1_white wid_185px">
                 <div className="number">{Number(summary?.data.activity?.email)}</div>
                 <div className="data">email</div>
               </div>
-              <div className="card">
+              <div className="card wid_185px">
                 <div className="number">{Number(summary?.data.activity?.orders)}</div>
                 <div className="data">orders</div>
               </div>
             </div>
           </div>
         </div>
-        <div className="grid_container_row" style={{ padding: '0px', backgroundColor: '#EDFFED' }}>
+        <div className="grid_container_row sms_carrer wid_225px">
           <div className="communication_activity_body">
-            <div className="title">90-DAY COMMUNICATION ACTIVITY</div>
+            <div className="title">SMS CARRIER STATUS</div>
             <div className="content">
               <div className="card">
                 <div className="number">{summary?.data?.carrier_status?.status}</div>
-                <div className="data" style={{ backgroundColor: '#D5E5D5' }}>since   {(Month[new Date(summary?.data?.carrier_status?.since).getMonth()]) + ' ' + (new Date(summary?.data?.carrier_status?.since).getDate()) + ', ' + (new Date(summary?.data?.carrier_status?.since).getFullYear())} </div>
+                <div className="data">since   {(Month[new Date(summary?.data?.carrier_status?.since).getMonth()]) + ' ' + (new Date(summary?.data?.carrier_status?.since).getDate()) + ', ' + (new Date(summary?.data?.carrier_status?.since).getFullYear())} </div>
               </div>
             </div>
           </div>
@@ -171,7 +171,7 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
             }
             <div className="recent_orders">
               <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
-              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(2)}>Errors</button>
+              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn mar_lr_20px' : 'tab_btn mar_lr_20px'} onClick={() => isOpenSubTabs(2)}>Errors</button>
               <div className="title">RECENT ORDERS</div>
             </div>
             <div className="no_data">No items</div>
@@ -184,8 +184,8 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
               <TabLoader /> : null
             }
             <div className="recent_orders">
-              <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
-              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(2)}>Errors</button>
+            <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
+              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn mar_lr_20px' : 'tab_btn mar_lr_20px'} onClick={() => isOpenSubTabs(2)}>Errors</button>
               <div className="title">RECENT ORDERS</div>
             </div>
             <div className="no_data">No items</div>
@@ -197,8 +197,8 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
               <TabLoader /> : null
             }
             <div className="recent_orders">
-              <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
-              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(2)}>Errors</button>
+            <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
+              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn mar_lr_20px' : 'tab_btn mar_lr_20px'} onClick={() => isOpenSubTabs(2)}>Errors</button>
 
               {orders?.data?.orders_AAA?.sent &&
                 <div className="title">RECENT ORDERS</div>
@@ -218,8 +218,8 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
               <TabLoader /> : null
             }
             <div className="recent_orders">
-              <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
-              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(2)}>Errors</button>
+            <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
+              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn mar_lr_20px' : 'tab_btn mar_lr_20px'} onClick={() => isOpenSubTabs(2)}>Errors</button>
               <div className="title">RECENT ORDERS</div>
             </div>
             <div className="no_data">No items</div>
@@ -231,8 +231,8 @@ const EvoTestDesign: FC<props>  = ({summary, orders}): ReactElement => {
               <TabLoader /> : null
             }
             <div className="recent_orders">
-              <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
-              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(2)}>Errors</button>
+            <button className={tabsub_button && subtabActive === 1 ? 'button_active tab_btn' : 'tab_btn'} onClick={() => isOpenSubTabs(1)}>Sent</button>
+              <button className={tabsub_button && subtabActive === 2 ? 'button_active tab_btn mar_lr_20px' : 'tab_btn mar_lr_20px'} onClick={() => isOpenSubTabs(2)}>Errors</button>
               <div className="title">RECENT ORDERS</div>
             </div>
             <div className="no_data">No items</div>
